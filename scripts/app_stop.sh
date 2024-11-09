193ff7d1970667c8493fcbac2cd9abb7caf654ed
Ava Williams <103846237+avareesew@users.noreply.github.com>
2024-11-09 09:27:06 -0700

    Update app_stop.sh

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# Navigate to the app directory
 cd /home/ec2-user/app
 
-# Find the gunicorn process and kill it
-pkill gunicorn
+# Attempt to stop Gunicorn if it's running
+if pgrep gunicorn > /dev/null
+then
+    pkill gunicorn
+    echo "Stopped Gunicorn process."
+else
+    echo "No Gunicorn process found."
+fi
+
