416ff71c494f910c4f8689eb42a55b9312f31a48
Ava Williams <103846237+avareesew@users.noreply.github.com>
2024-11-09 09:22:38 -0700

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+# Navigate to the app directory
 cd /home/ec2-user/app
 
 # Activate the virtual environment
 source venv/bin/activate
 
-# Start the Flask app with Gunicorn (assuming your app is in app.py)
+# Start the Flask app with Gunicorn
 gunicorn --workers 3 --bind 0.0.0.0:8000 app:app
 
